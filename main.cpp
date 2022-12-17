5fac03397afb7bee896af9b2ae5378abffd2122b
Cristobal Andrés Ramirez González <cramirezg@utem.cl>
2022-12-16 23:00:10 -0300

    Ajustes formato y entrada de datos
     Ajuste de formato y entrada de fecha por línea de comandos

--- main.cpp
+++ main.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+// Función que convierte una fecha a un valor double
+double dateToDouble(int day, int month, int year)
+{
+    // Creamos una estructura tm para representar la fecha
+    tm date;
+    date.tm_mday = day;
+    date.tm_mon = month - 1; // Los meses van de 0 a 11
+    date.tm_year = year - 1900; // Los años se cuentan a partir de 1900
+    date.tm_hour = 0; // Inicializamos la hora a 0
+    date.tm_min = 0; // Inicializamos los minutos a 0
+    date.tm_sec = 0; // Inicializamos los segundos a 0
+    
+
+    // Convertimos la fecha a un valor double utilizando la función mktime
+    time_t timestamp = mktime(&date);
+
+    // Convertimos el timestamp a un valor double y lo devolvemos
+    return static_cast<double>(timestamp);
+}
+
+
 // Función para calcular la pendiente de la recta
 double calculateSlope(vector<double> dates, vector<double> accidents)
 {
@@ -65,6 +86,7 @@ return intercept;
 
 int main(int argc, char* argv[])
 {
+    int day, month, year;
     // Inicializamos OpenMPI
     MPI_Init(&argc, &argv);
 
@@ -95,9 +117,29 @@ int main(int argc, char* argv[])
         string date_str = line.substr(0, pos);
         string accident_str = line.substr(pos + 1);
         // Convertimos las dos partes a valores numéricos y los almacenamos en los vectores de datos
-        double date = stod(date_str);
+
+        
+        //Separa la fecha en año mes y dia para luego convertir el valor resultante en un valor double
+        size_t pos2 = date_str.find("-");
+        string year_str = date_str.substr(0, pos2);
+        year = stoi(year_str);
+        pos2 += 1;
+
+        pos2 = date_str.find("-", pos2);
+        string month_str = date_str.substr(pos2 - 2);
+        month = stoi(month_str);
+        pos2 += 1;
+        
+        string day_str = date_str.substr(pos2);
+        day = stoi(day_str);
+
+        //Ajusta el valor de la fecha mediante la funcion dateToDoble
+        double date_double = dateToDouble(day,month,year);
+        //cout<<"dia: " <<day << "mes: " <<month <<"año: " <<year <<"valorDoble: "<<date_double<<endl;
+
+
         double accident = stod(accident_str);
-        dates.push_back(date);
+        dates.push_back(date_double);
         accidents.push_back(accident);
         }
         else{
@@ -128,20 +170,51 @@ int main(int argc, char* argv[])
     // Mostramos el resultado por pantalla por el proceso 0
     if (world_rank == 0)
     {
-    cout << "La pendiente de la recta que mejor se ajusta a los datos es: " << slope << endl;
-    cout << "El punto de intersección con el eje y (la constante) es: " << intercept << endl;
+
+    //cout << "La pendiente de la recta que mejor se ajusta a los datos es: " << slope << endl;
+    //cout << "El punto de intersección con el eje y (la constante) es: " << intercept << endl;
+    cout << "Para obtener datos correctos la fecha debe estar en el formato ISO 8601: DD-MM-YYYY" << endl;
+    //combierte el primer argumento de la linea de comandos a un string
+    string arg1(argv[1]);
+
+    //Separa la fecha en año mes y dia para luego convertir el valor resultante en un valor double
+    size_t pos2 = arg1.find("-");
+    string year_str = arg1.substr(0, pos2);
+    year = stoi(year_str);
+    pos2 += 1;
+
+    pos2 = arg1.find("-", pos2);
+    string month_str = arg1.substr(pos2 - 2);
+    month = stoi(month_str);
+    pos2 += 1;
+        
+    string day_str = arg1.substr(pos2);
+    day = stoi(day_str);
+
+    //Ajusta el valor de la fecha mediante la funcion dateToDoble 
+    //validador para que los meses no sean negativos
+    double date_arg1; 
+        if(month >= 10){
+            cout<< year << "-" << month  << "-" << day;
+            date_arg1 = dateToDouble(year,month,day);
+        }
+        else{
+            if(month > 0){
+                cout<< year << "-0" << month  << "-" << day;
+                date_arg1 = dateToDouble(year,month,day);
+            }
+            else{
+
+                cout<<"El formato ingresado es incorrecto" << endl;
+                return 1;
+            }
+        }
 
 
+    // Calculamos la predicción utilizando la ecuación de la recta con los valores determinados por las distintas maquinas (y = mx + b)
+    double prediction = slope * date_arg1 + intercept;
 
-    // Pedimos al usuario que ingrese una fecha futura para la predicción
-    double date;
-    cout << "Ingrese una fecha futura (en el mismo formato que los datos históricos): ";
-    cin >> date;
-
-    // Calculamos la predicción utilizando la ecuación de la recta con los valores determinados por las distintas maquinas
-    double prediction = slope * date + intercept;
-
-    cout << "Cantidad de accidentes esperados para la fecha ingresada: " << endl << prediction << endl;
+    cout << " // Cantidad de accidentes esperados: " << prediction << endl;
     }
 
 
