1b29bf863e797b2c6560f869c9abaceacc6e48b4
Cristobal Andrés Ramirez González <cramirezg@utem.cl>
2022-12-18 20:24:30 -0300

    Lectura del archivo mediante Send y Recv
    Ahora el vector que posee los datos del fecha y accidentes esta en el proceso 0, de tal forma que los demás procesos enviar información a este mediante MPI_send  y MPI_recv

--- main.cpp
+++ main.cpp
@@ -7,8 +7,7 @@
 using namespace std;
 
 // Función que convierte una fecha a un valor double
-double dateToDouble(int day, int month, int year)
-{
+double dateToDouble(int day, int month, int year){
     // Creamos una estructura tm para representar la fecha
     tm date;
     date.tm_mday = day;
@@ -18,7 +17,6 @@ double dateToDouble(int day, int month, int year)
     date.tm_min = 0; // Inicializamos los minutos a 0
     date.tm_sec = 0; // Inicializamos los segundos a 0
     
-
     // Convertimos la fecha a un valor double utilizando la función mktime
     time_t timestamp = mktime(&date);
 
@@ -26,58 +24,54 @@ double dateToDouble(int day, int month, int year)
     return static_cast<double>(timestamp);
 }
 
-
 // Función para calcular la pendiente de la recta
-double calculateSlope(vector<double> dates, vector<double> accidents)
-{
-// inicialización de las variables necesarias para la fórmula de la pendiente
-int n = dates.size();
-double sum_x = 0;
-double sum_y = 0;
-double sum_xy = 0;
-double sum_x2 = 0;
+double calculateSlope(vector<double> dates, vector<double> accidents){
+    // inicialización de las variables necesarias para la fórmula de la pendiente
+    int n = dates.size();
+    double sum_x = 0;
+    double sum_y = 0;
+    double sum_xy = 0;
+    double sum_x2 = 0;
 
-// Recorremos los vectores de datos y calculamos los valores necesarios para la fórmula de la pendiente
-for (int i = 0; i < n; i++)
-{
-    double x = dates[i];
-    double y = accidents[i];
+    // Recorremos los vectores de datos y calculamos los valores necesarios para la fórmula de la pendiente
+    for (int i = 0; i < n; i++)
+    {
+        double x = dates[i];
+        double y = accidents[i];
 
-    sum_x += x;
-    sum_y += y;
-    sum_xy += x * y;
-    sum_x2 += x * x;
-}
+        sum_x += x;
+        sum_y += y;
+        sum_xy += x * y;
+        sum_x2 += x * x;
+    }
 
-// Calculamos la pendiente utilizando la fórmula
-double slope = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x * sum_x);
+    // Calculamos la pendiente utilizando la fórmula
+    double slope = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x * sum_x);
 
-return slope;
+    return slope;
 }
 
 // Función para calcular el punto de intersección con el eje y (la constante)
-double calculateIntercept(vector<double> dates, vector<double> accidents, double slope)
-{
-// Inicializamos las variables necesarias para la fórmula de la constante
-int n = dates.size();
-double sum_x = 0;
-double sum_y = 0;
+double calculateIntercept(vector<double> dates, vector<double> accidents, double slope){
+    // Inicializamos las variables necesarias para la fórmula de la constante
+    int n = dates.size();
+    double sum_x = 0;
+    double sum_y = 0;
 
+    // Recorremos los vectores de datos y calculamos los valores necesarios para la fórmula de la constante
+    for (int i = 0; i < n; i++)
+    {
+        double x = dates[i];
+        double y = accidents[i];
 
-// Recorremos los vectores de datos y calculamos los valores necesarios para la fórmula de la constante
-for (int i = 0; i < n; i++)
-{
-    double x = dates[i];
-    double y = accidents[i];
+        sum_x += x;
+        sum_y += y;
+    }
 
-    sum_x += x;
-    sum_y += y;
-}
+    // Calculamos la constante utilizando la fórmula
+    double intercept = (sum_y - slope * sum_x) / n;
 
-// Calculamos la constante utilizando la fórmula
-double intercept = (sum_y - slope * sum_x) / n;
-
-return intercept;
+    return intercept;
 }
 
 
@@ -87,10 +81,12 @@ return intercept;
 int main(int argc, char* argv[])
 {
     int day, month, year;
+
     // Inicializamos OpenMPI
     MPI_Init(&argc, &argv);
+    MPI_Status estado;
 
-   // Obtenemos el número total de procesos y el índice del proceso actual
+    // Obtenemos el número total de procesos y el índice del proceso actual
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     int world_rank;
@@ -109,16 +105,16 @@ int main(int argc, char* argv[])
     // Leemos las líneas restantes del archivo y las procesamos
     string line;
     while (getline(file, line)){
-        // Si el índice del proceso actual es 0, procesamos la línea
-        if (world_rank == 0)
+
+        // Si el índice del proceso actual es distinto de 0, procesamos la línea y la enviamos a al proceso 0
+        if (world_rank != 0)
         {
+    
         // Separamos la línea en dos partes, utilizando el carácter ";" como separador
         size_t pos = line.find(";");
         string date_str = line.substr(0, pos);
         string accident_str = line.substr(pos + 1);
-        // Convertimos las dos partes a valores numéricos y los almacenamos en los vectores de datos
 
-        
         //Separa la fecha en año mes y dia para luego convertir el valor resultante en un valor double
         size_t pos2 = date_str.find("-");
         string year_str = date_str.substr(0, pos2);
@@ -135,14 +131,28 @@ int main(int argc, char* argv[])
 
         //Ajusta el valor de la fecha mediante la funcion dateToDoble
         double date_double = dateToDouble(day,month,year);
-        //cout<<"dia: " <<day << "mes: " <<month <<"año: " <<year <<"valorDoble: "<<date_double<<endl;
 
-
+        //Ajusta el valor de los accidentes para que se maneje como un valor double
         double accident = stod(accident_str);
-        dates.push_back(date_double);
-        accidents.push_back(accident);
+    
+        // envia los valores a el proceso 0 con los tag 0 y 1 respectivamente
+        MPI_Send(&date_double, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&accident, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);   
+        
+
         }
-        else{
+        else{   //Proceso 0
+
+            double date,accident_double;
+            for (int Proceso = 1; Proceso < world_size; Proceso++)
+            {
+                MPI_Recv(&date, 1, MPI_DOUBLE, Proceso, 0, MPI_COMM_WORLD, &estado);
+                MPI_Recv(&accident_double, 1, MPI_DOUBLE, Proceso, 1, MPI_COMM_WORLD, &estado);
+                //Almacena los datos de las fechas y accidentes en un vector dentro del proceso 0
+                dates.push_back(date);
+                accidents.push_back(accident_double);
+            }
+            
            
         }
 
@@ -151,30 +161,36 @@ int main(int argc, char* argv[])
     // Cerramos el archivo
     file.close();
 
+    //MPI_Barrier Se asegura de que toda la sección anterior allá finalizado
+    MPI_Barrier(MPI_COMM_WORLD);
+
     // Calculamos la pendiente de la recta que mejor se ajusta a los datos con la maquina principal
-    double slope = 0;
+    double slope;
+    double intercept;
     if (world_rank == 0)
     {
+    //utilizando los datos de los vectores almacenados en el proceso 0 calcula 
     slope = calculateSlope(dates, accidents);
+
+    // Calculamos el punto de intersección con el eje y (la constante)
+    intercept = calculateIntercept(dates, accidents, slope);
     }
 
     // Enviamos la pendiente a los demás procesos/maquinas
     MPI_Bcast(&slope, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&intercept, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Calculamos el punto de intersección con el eje y (la constante)
-    double intercept = calculateIntercept(dates, accidents, slope);
-
-
-
+    //MPI_Barrier Se asegura de que toda la sección anterior allá finalizado
+    MPI_Barrier(MPI_COMM_WORLD);
 
     // Mostramos el resultado por pantalla por el proceso 0
     if (world_rank == 0)
     {
-
     //cout << "La pendiente de la recta que mejor se ajusta a los datos es: " << slope << endl;
     //cout << "El punto de intersección con el eje y (la constante) es: " << intercept << endl;
     cout << "Para obtener datos correctos la fecha debe estar en el formato ISO 8601: DD-MM-YYYY" << endl;
-    //combierte el primer argumento de la linea de comandos a un string
+
+    //convierte el primer argumento de la línea de comandos a un String
     string arg1(argv[1]);
 
     //Separa la fecha en año mes y dia para luego convertir el valor resultante en un valor double
@@ -191,11 +207,11 @@ int main(int argc, char* argv[])
     string day_str = arg1.substr(pos2);
     day = stoi(day_str);
 
-    //Ajusta el valor de la fecha mediante la funcion dateToDoble 
-    //validador para que los meses no sean negativos
+    //Validador para que los meses no sean negativos
     double date_arg1; 
         if(month >= 10){
             cout<< year << "-" << month  << "-" << day;
+            //Ajusta el valor de la fecha mediante la funcion dateToDoble 
             date_arg1 = dateToDouble(year,month,day);
         }
         else{
@@ -216,7 +232,6 @@ int main(int argc, char* argv[])
 
     cout << " // Cantidad de accidentes esperados: " << prediction << endl;
     }
-
 
     // Finalizamos OpenMPI
     MPI_Finalize();
